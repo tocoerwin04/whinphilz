8924009abbf95c2883245dba5c9bad39da197b79
tocoerwin04 <tocoerwin04@gmail.com>
2026-09-13 00:07:24 +0800

    Update Project21.java

--- src/Project20/Project21.java
+++ src/Project20/Project21.java
@@ -4,16 +4,16 @@ public class Project21 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Prompt user for name input
+        // Prompt user for input
         System.out.print("Enter your name: ");
         String name = scanner.nextLine();
         
-        //input
         System.out.print("Enter first number: ");
         double num1 = scanner.nextDouble();
 
         System.out.print("Enter second number: ");
         double num2 = scanner.nextDouble();
+        
         //output
         if (num1 > num2) {
             System.out.println("The highest number is: " + num1);
